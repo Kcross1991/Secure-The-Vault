20b917ac220cce229ec3eaed1567c989bee33150
root <root@Kacy>
2023-11-06 13:54:21 -0600

    SecureJava

---
+++ vault.js
@@ -0,0 +1,13 @@
+//Pseudocode: Secure cthe Vault steps.
+// Step 1: add 5+5 =10
+// Step 2: Multipy 5*8 =40
+// Step 3: Divide _/_ =39
+// Step 4: Add a dialoge box by using alert
+
+
+
+const strg= "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+const ten = 5+5;
+const forty = 5*8;
+const thirtyNine = 60-21;
+alert(strg+ "  "+ten+ "  " +forty+ " " +thirtyNine);
