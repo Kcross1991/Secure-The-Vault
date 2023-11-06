b31321caf263b91cbc0a9d08a17f2f60820654db
root <root@Kacy>
2023-11-06 14:13:08 -0600

    SecureJava

--- vault.js
+++ vault.js
@@ -1,8 +1,9 @@
 //Pseudocode: Secure thhe Vault steps.
-// Step 1: add 5+5 =10
-// Step 2: Multipy 5*8 =40
-// Step 3: Divide _/_ =39
-// Step 4: Add a dialoge box by using alert
+//Step 1: Add variable with the message that is provided.
+//Step 2: Add variable 1: Add 5+5 =10
+//Step 3: Add variable 2: Multipy 5*8 =40
+//Step 4: Add variable 3: Subtract 60-21 =39
+//Step 5: Add a dialoge box by using alert
 
 
 // I typed in the message with the strng variable.
@@ -10,6 +11,6 @@ const strg= "You have received this message because you have been chosen to open
 const String1 = 5+5;
 const String2 = 5*8;
 const String3 = 60-21;
-// I added a popup dialogg box with the 4 above strings incorporated 
+// I added a popup dialog box with the 4 above strings incorporated 
 alert(strg+ "  "+String1+ "  " +String2+ " " +String3);
 console.log(strg)
