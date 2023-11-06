28e86388d23cca50a019bab43a5e94a76f2e0f31
root <root@Kacy>
2023-11-06 14:08:54 -0600

    SecureJava

--- vault.js
+++ vault.js
@@ -5,10 +5,11 @@
 // Step 4: Add a dialoge box by using alert
 
 
-
+// I typed in the message with the strng variable.
 const strg= "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
 const String1 = 5+5;
 const String2 = 5*8;
 const String3 = 60-21;
+// I added a popup dialogg box with the 4 above strings incorporated 
 alert(strg+ "  "+String1+ "  " +String2+ " " +String3);
 console.log(strg)
